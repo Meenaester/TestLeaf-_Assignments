2098a3ec0f377c9985e212d11693668db23c9dc8
Meenasathish <meenasel347@gmail.com>
2026-08-21 02:36:39 +0530

    Add files via upload

---
+++ day 2 assignment_oddOrEven.js
@@ -0,0 +1,25 @@
+
+// Assignment Details:
+// Write a JavaScript function named `isOddOrEven` that takes an integer as input and returns `Odd` if the
+// number is odd and `"Even"` if the number is even.
+// Assignment Requirements:
+// 1. Create a function named `isOddOrEven` that takes a number as a parameter
+// 2. Declare and initialize the variable
+// 3. Use a conditional statement to check if the number is divisible by 2
+// 4. Call the function and print the result
+
+
+
+function isOddOrEven (number)
+{
+if(number%2==0)
+{
+    console.log('This is a even number')
+}
+else
+{
+    console.log('This is a odd number')
+}
+}
+isOddOrEven(12)
+isOddOrEven(3)
